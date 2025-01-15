1d63291f6f51794c7e168f9022c43a39a28ca034
RahafJazar <rahafjazar73@gmail.com>
2025-01-15 19:57:54 +0300

    Ayman Added A printMessage method

---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student{
+	public static void main (String [] args){
+		printMessage("Welcomme Ahmad ") ;
+
+	}
+	public static void printMessage(String msg){
+		System.out.println (msg);
+	}
+}
+
