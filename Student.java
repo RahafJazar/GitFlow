a6e1a0a78c2422ed84c26209713ea602445e32cd
RahafJazar <rahafjazar73@gmail.com>
2025-01-15 20:17:55 +0300

    Khaled added a getAge Method

--- Student.java
+++ Student.java
@@ -1,10 +1,15 @@
 public class Student{
+	int age =20;
 	public static void main (String [] args){
 		printMessage("Welcomme Ahmad ") ;
-
+                Student s1= new Student();
+		System.out.println(s1.getAge());
 	}
 	public static void printMessage(String msg){
 		System.out.println (msg);
+	}
+	public int getAge(){
+		return age;
 	}
 }
 
